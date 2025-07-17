04389ead59e1928ae9ac908981607823dbfa36e7
Ritik <sritik7256@gmail.com>
2025-07-17 14:30:21 +0530

    data transformation

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,22 @@
+
+import numpy as np
+import os
 import sys
-
-from src.logger import logging
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name[{0}] line number[{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
-    
-    return error_message
-
-    
-
-class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
-    def __str__(self):
-        return self.error_message
+import pandas as pd
+import dill
 
 
+from src.exception import CustomException
 
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+           
